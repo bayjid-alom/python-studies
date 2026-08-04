c92dc60e401dcad31ee3ce2c3afc76120d8d81f1
bayjidalom <bayjidalom1005@gmail.com>
2026-08-04 12:57:03 +0600

    introduce Python logging basics

---
+++ Chapter-08/01_import_logging.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info(" Program started.")
+logging.warning(" Watch out!!")
+logging.info(" Program finished.")
+
+
+"""
+INFO:root: Program started.
+WARNING:root: Watch out!!
+INFO:root: Program finished.
+"""
