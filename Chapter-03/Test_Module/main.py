b44fca4187cba7c73a1ba48184f860be06e8cf2b
bayjidalom <bayjidalom1005@gmail.com>
2026-08-06 13:14:32 +0600

    implement module import example

---
+++ Chapter-03/Test_Module/main.py
@@ -0,0 +1,4 @@
+import testmodule
+
+print("Addition..........: ", testmodule.add_func(10, 20))
+print("Subtraction.......: ", testmodule.sub_func(10, 20))
---
+++ Chapter-03/Test_Module/testmodule.py
@@ -0,0 +1,8 @@
+# Function definition for addition
+def add_func(a, b):
+    return a + b
+
+
+# Function definition for subtraction
+def sub_func(a, b):
+    return a - b
