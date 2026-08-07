a8a38d7ba44121b5f9673670d5a83fa50ea5a6fe
bayjidalom <bayjidalom1005@gmail.com>
2026-08-07 22:40:41 +0600

    Add Python import module with student info

---
+++ Chapter-03/Student_Module/main.py
@@ -0,0 +1,3 @@
+import student
+
+student.show_info()
---
+++ Chapter-03/Student_Module/student.py
@@ -0,0 +1,9 @@
+name = "Bayjid"
+age = 18
+status = "Single"
+
+
+def show_info():
+    print(f"Name :", name)
+    print(f"Age :", age)
+    print(f"Status :", status)
