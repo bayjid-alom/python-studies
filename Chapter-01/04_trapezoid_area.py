52c3aba6093c72acb7c890e09a5f18194b78e0f2
bayjidalom <bayjidalom1005@gmail.com>
2026-08-08 22:36:27 +0600

    Add trapezoid area calculation

---
+++ Chapter-01/04_trapezoid_area.py
@@ -0,0 +1,26 @@
+a = float(input("Enter first base: "))
+b = float(input("Enter second base: "))
+height = float(input("Enter height: "))
+
+area = 0.5 * (a + b) * height
+
+print("Trapezoid Area:", area)
+
+
+"""
+Output 01 :
+
+Enter first base: 40
+Enter second base: 20
+Enter height: 8
+Trapezoid Area: 240.0
+
+
+Output 02 :
+Enter first base: 16
+Enter second base: 40
+Enter height: 13
+Trapezoid Area: 364.0
+
+
+"""
