49a59053b89afc2724e86ca156bccbcb07e7240c
bayjidalom <bayjidalom1005@gmail.com>
2026-08-08 18:42:35 +0600

    Add number validation with raise

---
+++ Chapter-07/03_raise_number.py
@@ -0,0 +1,18 @@
+number = -5
+
+if number < 0:
+    raise ValueError("Number cannot be negative")
+
+print("Positive number")
+
+
+
+
+"""
+Traceback (most recent call last):
+  File "C:\Python-Programming\Chapter-07\03_raise_number.py", line 4, in <module>
+    raise ValueError("Number cannot be negative")
+ValueError: Number cannot be negative
+"""
+
+
