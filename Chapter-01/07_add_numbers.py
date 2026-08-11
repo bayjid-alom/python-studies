4c25b9fae2d9f5a81c569bb992adb30591d3fe39
bayjidalom <bayjidalom1005@gmail.com>
2026-08-11 19:37:49 +0600

    add function for sum calculation

---
+++ Chapter-01/07_add_numbers.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+result = add(num1, num2)
+print("Summation is = ", result)
+
+
+"""
+Enter the first number : 55
+Enter the second number : 45
+Summation is =  100
+"""
