ea8f9f9379cc51a9a0a2e0a776007b39fed6ce1f
bayjidalom <bayjidalom1005@gmail.com>
2026-08-12 18:25:18 +0600

    feat: add square module

---
+++ Chapter-03/Square_Module/main.py
@@ -0,0 +1,13 @@
+import square
+
+result = square.square(int(input("Enter a number = ")))
+print("The square value is = ", result)
+
+
+"""
+Enter a number = 4
+The square value is =  16
+
+Enter a number = 10
+The square value is =  100
+"""
