e24d5fbd85bcd380372fdecea0b7fd452b56945e
bayjidalom <bayjidalom1005@gmail.com>
2026-08-12 18:17:51 +0600

    feat: add cube function

---
+++ Chapter-01/09_cube_function.py
@@ -0,0 +1,17 @@
+def cube(number):
+    return number * number * number
+
+
+num = int(input("Enter a number: "))
+
+print("Cube:", cube(num))
+
+
+""""
+Enter a number: 10
+Cube: 1000
+
+
+Enter a number: 4
+Cube: 64
+"""
